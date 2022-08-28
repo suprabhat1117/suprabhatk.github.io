ae8fbfb09a0bee6a5986964dbd17607ed923da97
Suprabhat Karmakar <suprabhatk1117@gmail.com>
2022-08-28 21:45:25 +0530

    THEME INTEGRATION

--- JS/script.js
+++ JS/script.js
@@ -8,6 +8,10 @@ const success = document.querySelector('#success');
 const errorNodes = document.querySelectorAll('.error');
 
 const navbar_logo = document.querySelector('#navbar__logo');
+const root_directory = '/suprabhatk.github.io/';
+
+let themeDots = document.getElementsByClassName('theme-dot');
+let theme = localStorage.getItem('theme');
 
 /* hamburger toggle*/
 menu.addEventListener('click',function(e){
@@ -26,8 +30,39 @@ menuLinks.addEventListener('click',function(e){
 /* logo redirect */
 navbar_logo.addEventListener('click',function(e){
     e.preventDefault();
-    window.location.href = 'https://suprabhat1117.github.io/suprabhatk.github.io/';
+    window.location.href = window.location.href + 'index.html';
 })
+
+/* theme change */
+if(theme == null){
+    setTheme('light-mode')
+}else{
+    setTheme(theme);
+}
+
+for(var i=0;i < themeDots.length;i++){
+    themeDots[i].addEventListener('click',function(){
+        let mode = this.dataset.mode;
+        setTheme(mode);
+    })
+}
+
+function setTheme(mode){
+    if(mode =='light-mode'){
+        document.getElementById('theme-style').href='CSS/default.css'
+    }
+    if(mode =='blue-mode'){
+        document.getElementById('theme-style').href='CSS/blue.css'
+    }
+    if(mode =='green-mode'){
+        document.getElementById('theme-style').href='CSS/green.css'
+    }
+    if(mode =='purple-mode'){
+        document.getElementById('theme-style').href='CSS/purple.css'
+    } 
+
+    localStorage.setItem('theme',mode);
+}
 
 // https://suprabhat1117.github.io/suprabhatk.github.io/
 // http://127.0.0.1:5500/index.html
